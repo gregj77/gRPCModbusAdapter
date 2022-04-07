f829d2ec861aacdbd06a732778a8a0c818559589
Greg <greg_j@live.com>
2022-04-07 22:59:01 +0200

    added missing function service

--- gRPCModbusAdapter.Service/src/main/kotlin/com/gcs/gRPCModbusAdapter/functions/ReadCurrentVoltageFunction.kt
+++ gRPCModbusAdapter.Service/src/main/kotlin/com/gcs/gRPCModbusAdapter/functions/ReadCurrentVoltageFunction.kt
@@ -3,7 +3,9 @@ package com.gcs.gRPCModbusAdapter.functions
 import com.gcs.gRPCModbusAdapter.functions.args.ReadCurrentVoltageFunctionArgs
 import com.gcs.gRPCModbusAdapter.functions.utils.MessageCRCServiceImpl
 import com.gcs.gRPCModbusAdapter.functions.utils.toShort
+import org.springframework.stereotype.Service
 
+@Service
 class ReadCurrentVoltageFunction(crcService: MessageCRCServiceImpl) : ModbusFunctionBase<ReadCurrentVoltageFunctionArgs, Float>(crcService, 7) {
     override val functionName: String
         get() = FunctionName
