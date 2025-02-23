e64210b6d1c23b20991e8e9d2f83da97bb693fac
Greg <greg_j@live.com>
2025-02-23 21:20:31 +0100

    added more logging + defer

--- gRPCModbusAdapter.Service/src/main/kotlin/com/gcs/gRPCModbusAdapter/functions/ModbusFunctionBase.kt
+++ gRPCModbusAdapter.Service/src/main/kotlin/com/gcs/gRPCModbusAdapter/functions/ModbusFunctionBase.kt
@@ -14,44 +14,48 @@ import reactor.util.retry.Retry
 import reactor.util.retry.Retry.RetrySignal
 import java.time.Duration
 import java.time.Instant
+import java.util.concurrent.atomic.AtomicInteger
 
 
 interface ModbusFunction {
     val functionName: String
 }
 
-abstract class ModbusFunctionBase<in TArgs : FunctionArgs, TResult>(private val crcService: MessageCRCService, private val responseMessageSize: Int, private val logger: KLogger) : ModbusFunction {
+val executionId = AtomicInteger(0)
+
+abstract class ModbusFunctionBase<in TArgs : FunctionArgs, TResult : Any>(private val crcService: MessageCRCService, private val responseMessageSize: Int, private val logger: KLogger) : ModbusFunction {
 
     fun execute(args: TArgs): Mono<TResult> {
-        logger.debug { "preparing message for ${args.deviceId} to query ${args.registerId} ..." }
+        val id = executionId.incrementAndGet()
+        logger.debug { "[$id] preparing message for ${args.deviceId} to query ${args.registerId} ..." }
         val request = args.toMessage { crcService.calculateCRC(it) }
-        logger.debug { "message for ${args.deviceId} to query ${args.registerId} - calculated ${request.size} bytes" }
+        logger.debug { "[$id] message for ${args.deviceId} to query ${args.registerId} - calculated ${request.size} bytes" }
         val response = ByteArray(responseMessageSize)
         val start = Instant.now().toEpochMilli()
-        return args.driver
-            .communicateAsync(request)
+        return Flux
+            .defer { args.driver.communicateAsync(request) }
             .take(response.size.toLong())
             .index()
             .collect({ response }, { buffer, valueAndIndex -> buffer[valueAndIndex.t1.toInt()] = valueAndIndex.t2 })
-            .map { extractOrThrow(args, it) }
-            .timeout(Duration.ofSeconds(15L))
-            .retryWhen(createRetryStrategy())
+            .flatMap { extractOrThrow(id, args, it) }
+            .retryWhen(createRetryStrategy(id))
+            .timeout(Duration.ofSeconds(5L))
             .doFinally {
                 if (it == SignalType.ON_COMPLETE || it == SignalType.ON_ERROR) {
                     val stop = Instant.now().toEpochMilli()
-                    logger.info { "${args.deviceId}.${args.registerId} function $functionName - completed with $it after ${stop - start} ms" }
+                    logger.info { "[$id] ${args.deviceId}.${args.registerId} function $functionName - completed with $it after ${stop - start} ms" }
                 }
             }
     }
 
-    private fun extractOrThrow(args: TArgs, response: ByteArray): TResult {
+    private fun extractOrThrow(id: Int, args: TArgs, response: ByteArray): Mono<TResult> {
         if (crcService.checkCrc(response)) {
             val result = extractValue(response)
-            logger.debug { "got valid response from ${args.deviceId} query ${args.registerId} -> [${response.size} bytes]: $result" }
-            return result
+            logger.debug { "[$id] got valid response from ${args.deviceId} query ${args.registerId} -> [${response.size} bytes]: $result, [${response.toHexString()}]" }
+            return Mono.just(result)
         }
-        logger.warn { "failed to validate CRC from ${args.deviceId} response query ${args.registerId} - [${response.toHexString()}]" }
-        throw CrcCheckError()
+        logger.warn { "[$id] failed to validate CRC from ${args.deviceId} response query ${args.registerId} - [${response.toHexString()}]" }
+        return Mono.error(CrcCheckError())
     }
 
     protected abstract fun extractValue(response: ByteArray): TResult
@@ -59,16 +63,16 @@ abstract class ModbusFunctionBase<in TArgs : FunctionArgs, TResult>(private val 
     private fun ByteArray.toHexString(): String {
         return joinToString(separator = " ") { "%02x".format(it) }
     }
-    private fun createRetryStrategy(): Retry {
+    private fun createRetryStrategy(id : Int): Retry {
         return Retry.from { companion: Flux<RetrySignal> ->
             companion.handle<Any> { retrySignal: RetrySignal, sink: SynchronousSink<Any> ->
                 val ctx: Context = sink.currentContext()
                 val left: Int = ctx.getOrDefault("retriesLeft", 2)!!
                 if (left > 0 && retrySignal.failure() is CrcCheckError) {
-                    logger.debug { "retrying request due to CrcCheck error" }
+                    logger.debug { "[$id] retrying request due to CrcCheck error" }
                     sink.next(Context.of("retriesLeft", left - 1, "lastError", retrySignal.failure()))
                 } else {
-                    logger.info { "retry quota exceeded - aborting call" }
+                    logger.info { "[$id] retry quota exceeded - aborting call" }
                     sink.error(Exceptions.retryExhausted("retries exhausted", retrySignal.failure()))
                 }
             }
